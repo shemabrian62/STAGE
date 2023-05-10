d0d4b51b26b32ebf1bf6cadf8c7cce1d161e36e8
shemabrian62 <121482665+shemabrian62@users.noreply.github.com>
2023-05-11 01:25:41 +0200

    Make updates

---
+++ store.js
@@ -0,0 +1,12 @@
+// Function to view item details
+function viewItem(itemId) {
+    // Redirect to the item's page or perform any other action
+    console.log("Viewing item:", itemId);
+  }
+  
+  // Function to add item to cart
+  function addToCart(itemId) {
+    // Add the item to the cart or perform any other action
+    console.log("Adding item to cart:", itemId);
+  }
+  
